30a87ab72a5a76a202aa905d984f08237d44f12e
Attaphat Chana <attaphart@gmail.com>
2025-03-19 21:56:16 +0700

    type command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -42,12 +42,14 @@ public class Main {
             }else if (map.getOrDefault(command[0],100) == 2) {
                 String message = "";
                 for (String dir:paths) {
-                    String ex = dir + command[1];
+                    String ex = dir + "/" + command[1];
+                    //System.out.println(ex);
                     if (Files.exists(Path.of(ex))){
                         //System.out.println(command[1] + " is " + ex);
                         message = command[1] + " is " + ex;
                         break;
                     }
+
                 }
                 if (message.isBlank()){
                     System.out.println(command[1] +": not found");
@@ -55,13 +57,6 @@ public class Main {
                     System.out.println(message);
                 }
 
-                if (!path.isBlank()){
-                    if (map.containsKey(command[1])){
-                        System.out.println(command[1] + " is a shell builtin" );
-                    }else{
-                        System.out.println(command[1] +": not found");
-                    }
-                }
 
             }else{
                 System.out.println(input + ": command not found");
